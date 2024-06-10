797ae75442cf5b28d7a874dace3472aeaa6f0b44
hoseok jang <hs.jang@mongta.co.kr>
2024-06-10 15:56:06 +0900

    init commit

--- src/main/java/com/example/redisstudy/RedisPubSubController.java
+++ src/main/java/com/example/redisstudy/RedisPubSubController.java
@@ -14,12 +14,14 @@ public class RedisPubSubController {
 
     @PostMapping("/send")
     public void sendMessage(@RequestParam(required = true) String channel, @RequestBody MessageDTO message) {
+        // 메시지 저장
         log.info("Redis pub MSG Channel = {}", channel);
         redisPubService.pubMsgChannel(channel, message);
     }
 
     @PostMapping("/cancel")
     public void cancelSubChannel(@RequestParam String channel) {
+        // 구독 취소
         redisPubService.cancelSubChannel(channel);
     }
 
